6c6a0bc5bf6362efcaa4f1c92bf03a491c6b08d3
TheOneDarkling <raphael.talleux@gmail.com>
2017-01-29 14:56:35 +0100

    Creation fenetre

--- PacMan.py
+++ PacMan.py
@@ -1,1 +1,17 @@
 from tkinter import *
+
+
+
+#=======================================================================
+# PROGRAMME PRINCIPAL ==================================================
+#=======================================================================
+
+
+fen = Tk()
+
+fen.title("PAC MAN")
+
+plateau = Canvas(fen, width=500, height= 500, bg="black")
+plateau.pack(side="top")
+
+fen.mainloop()
