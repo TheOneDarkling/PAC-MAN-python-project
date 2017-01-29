8df36e61fc807b325e9e20e95266c7afdc09b449
TheOneDarkling <raphael.talleux@gmail.com>
2017-01-29 16:07:37 +0100

    Creation plateau(liste) + Varia de gestion +rafrech de l'ecran

--- PacMan.py
+++ PacMan.py
@@ -2,16 +2,55 @@ from tkinter import *
 
 
 
+	
+	
+def afficherJeu():
+	print(jeu)
+	
+
+
+def play():
+	global isPlay
+	
+	# Cette partie est rafraichie a 60fps
+	
+	
+	#Gere la fin
+	if(isPlay):
+		fen.after(17, play)
+
+
 #=======================================================================
 # PROGRAMME PRINCIPAL ==================================================
 #=======================================================================
 
+#Varibles utiles
+tailleTile = 16
+nbTilesHauteur = 30
+nbTilesLargeur = 30
 
+hauteurAffichage = tailleTile * nbTilesHauteur
+largeurAffichage = tailleTile * nbTilesLargeur
+
+isPlay = True
+
+
+jeu = [[0] * nbTilesLargeur for _ in range(nbTilesHauteur)]
+
+#Gestion plateau
+afficherJeu()
+
+
+#Gestion fenetre
 fen = Tk()
-
 fen.title("PAC MAN")
 
-plateau = Canvas(fen, width=500, height= 500, bg="black")
-plateau.pack(side="top")
+affichage = Canvas(fen, width=largeurAffichage, height= hauteurAffichage, bg="black")
+affichage.pack(side="top")
+
+#Gestion menu
+playButton = Button(fen, text="Play", command=play)
+playButton.pack(side=RIGHT)
+
 
 fen.mainloop()
